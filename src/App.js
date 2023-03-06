5afa60191ded170ae7385a3a9bf238a7e14f8583
Heung-e-ya <88697847+Heung-e-ya@users.noreply.github.com>
2023-03-06 18:19:22 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import Toc from "./components/Toc";
+import Subject from "./components/Subject";
+import Content from "./components/Content";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Subject title="WEB" sub="world wide web!" />
+      <Subject title="REACT" sub="For UI!" />
+      <Toc />
+      <Content title="HTML" desc="HTML is HyperText Markup Language!" />
     </div>
   );
 }
