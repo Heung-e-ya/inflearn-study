3fb3f02f7b095872aa1a4d5af5eed450ff1d02d5
Heung-e-ya <88697847+Heung-e-ya@users.noreply.github.com>
2023-03-09 17:40:54 +0900

    state

--- src/App.js
+++ src/App.js
@@ -5,15 +5,43 @@ import Subject from "./components/Subject";
 import Content from "./components/Content";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <Subject title="WEB" sub="world wide web!" />
-      <Subject title="REACT" sub="For UI!" />
-      <Toc />
-      <Content title="HTML" desc="HTML is HyperText Markup Language!" />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "read",
+      subject: { title: "WEB", sub: "world wid web!" },
+      welcome: { title: "Welcom", desc: "hello, React!!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "Css is for design" },
+        { id: 3, title: "Javascript", desc: "Javascript is for interactive" },
+      ],
+    };
+  }
+
+  render() {
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
+      <div>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+
+        <Toc data={this.state.contents} />
+        <Content title={_title} desc={_desc} />
+      </div>
+    );
+  }
 }
 
 export default App;
